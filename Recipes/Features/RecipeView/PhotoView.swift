f6df53a41b65029ba3a2b361c4182200fd01e9f0
Tony Short <tshort15@ford.com>
2023-04-10 10:18:36 +0100

    Adds button to method image zoom screen

--- Recipes/Features/RecipeView/PhotoView.swift
+++ Recipes/Features/RecipeView/PhotoView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct PhotoView: View {
-
+    @Environment(\.dismiss) var dismiss
     @State var scale: CGFloat = 1
     @State var scaleAnchor: UnitPoint = .center
     @State var lastScale: CGFloat = 1
@@ -19,48 +19,57 @@ struct PhotoView: View {
     let image: UIImage
 
     var body: some View {
-        GeometryReader { geometry in
-            let magnificationGesture = MagnificationGesture()
-                .onChanged{ gesture in
-                    scaleAnchor = .center
-                    scale = max(1, lastScale * gesture)
-                }
-                .onEnded { _ in
-                    fixOffsetAndScale(geometry: geometry)
+        NavigationView {
+            GeometryReader { geometry in
+                let magnificationGesture = MagnificationGesture()
+                    .onChanged{ gesture in
+                        scaleAnchor = .center
+                        scale = max(1, lastScale * gesture)
+                    }
+                    .onEnded { _ in
+                        fixOffsetAndScale(geometry: geometry)
+                    }
+
+                let dragGesture = DragGesture()
+                    .onChanged { gesture in
+                        var newOffset = lastOffset
+                        newOffset.width += gesture.translation.width
+                        newOffset.height += gesture.translation.height
+                        offset = newOffset
+                    }
+                    .onEnded { _ in
+                        fixOffsetAndScale(geometry: geometry)
+                    }
+
+                let doubleTapGesture = TapGesture(count: 2).onEnded {
+                    withAnimation(.easeIn) {
+                        let min = 1.0
+                        let max = 2.0
+                        if scale <= min { scale = max } else
+                        if scale >= max { scale = min } else {
+                            scale = ((max - min) * 0.5 + min) < scale ? max : min
+                        }
+                    }
                 }
 
-            let dragGesture = DragGesture()
-                .onChanged { gesture in
-                    var newOffset = lastOffset
-                    newOffset.width += gesture.translation.width
-                    newOffset.height += gesture.translation.height
-                    offset = newOffset
-                }
-                .onEnded { _ in
-                    fixOffsetAndScale(geometry: geometry)
-                }
-
-            let doubleTapGesture = TapGesture(count: 2).onEnded {
-                withAnimation(.easeIn) {
-                    let min = 1.0
-                    let max = 2.0
-                    if scale <= min { scale = max } else
-                    if scale >= max { scale = min } else {
-                        scale = ((max - min) * 0.5 + min) < scale ? max : min
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .position(x: geometry.size.width / 2,
+                              y: geometry.size.height / 2)
+                    .scaleEffect(scale, anchor: scaleAnchor)
+                    .offset(offset)
+                    .gesture(dragGesture)
+                    .gesture(doubleTapGesture)
+                    .gesture(magnificationGesture)
+            }
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Close") {
+                        dismiss()
                     }
                 }
             }
-
-            Image(uiImage: image)
-                .resizable()
-                .scaledToFit()
-                .position(x: geometry.size.width / 2,
-                          y: geometry.size.height / 2)
-                .scaleEffect(scale, anchor: scaleAnchor)
-                .offset(offset)
-                .gesture(dragGesture)
-                .gesture(doubleTapGesture)
-                .gesture(magnificationGesture)
         }
         .background(Color.black)
         .edgesIgnoringSafeArea(.all)
